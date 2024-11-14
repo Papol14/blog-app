1b172ddb670cf134c45ea062a99fd92c96c7db8b
Papol14 <johnpaulescalona2@gmail.com>
2024-11-14 20:57:38 +0800

    Initial commit

---
+++ src/pages/About.jsx
@@ -0,0 +1,11 @@
+const About = () => {
+  return (
+    <div className="mb-20">
+      <h1 className="sm:text-4xl text-2xl font-bold my-6 text-gray-900">
+        About
+      </h1>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,28 @@
+const Home = () => {
+  return (
+    <div className="mb-20">
+      <h1 className="sm:text-4xl text-2xl font-bold my-6 text-gray-900">
+        Hello Blog
+      </h1>
+      <p className="mx-auto leading-relaxed text-base mb-4">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Atque
+        aspernatur quod laudantium commodi facere, ipsam illo similique fugiat
+        omnis nostrum soluta ratione provident qui dolores rerum quibusdam alias
+        quidem perferendis?
+      </p>
+      <p className="mx-auto leading-relaxed text-base mb-4">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatibus
+        voluptates quas ut odio autem asperiores modi natus amet hic sequi,
+        doloremque sunt quos cum quam eos, in earum, fugit animi.
+        <p className="mx-auto leading-relaxed text-base mb-4">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus
+          placeat optio, error iure similique nihil reiciendis ab voluptatibus
+          veniam, molestiae cumque ratione omnis expedita. Laudantium optio
+          dolorum adipisci minus corporis.
+        </p>
+      </p>
+    </div>
+  );
+};
+
+export default Home;
