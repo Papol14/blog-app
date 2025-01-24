5f62a5b5571e33e4df40ad979766ae27e5a7316f
johnpaulescalona <112893924+Papol14@users.noreply.github.com>
2025-01-24 22:43:57 +0800

    Delete src/pages/NotFound.jsx

--- src/pages/NotFound.jsx
+++
@@ -1,9 +0,0 @@
-const NotFound = () => {
-  return (
-    <div className="sm:text-xl text-2-xl font-bold my-6 text-gray-900">
-      404: Page not found hehe
-    </div>
-  );
-};
-
-export default NotFound;
